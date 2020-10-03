409dfb07601120bef0f13e3e7543146e101d0449
Adam Long <adam.long.tech.recruiting@gmail.com>
2020-10-02 22:43:09 -0600

    Removed metadata from trade journal. Replaced with amount. Added another sheet for prepping trades.

--- trade_journal.py
+++ trade_journal.py
@@ -29,10 +29,12 @@ class TradeJournal():
 		ss = ezsheets.createSpreadsheet(self.title)
 		queued_trades = ss[0]
 		queued_trades.title = "Queued Trades"
-		queued_trades.updateRow(1, ['Ticker', 'Type', 'Entry Price', 'Exit Price', 'Stop Loss', 'Notes', 'Expiration in Days', 'Metadata', 'Sell At End of Day'])
+		queued_trades.updateRow(1, ['Ticker', 'Type', 'Entry Price', 'Exit Price', 'Stop Loss', 'Notes', 'Expiration in Days', 'Amount', 'Sell At End of Day'])
 		trades = ss.createSheet('Trades')
 		trades.updateRow(1, ['ID', 'Create Date', 'Ticker', 'Type', 'Status', 'Entry Date', 'Exit Date', 'Planned Entry Price', 'Planned Exit Price', 
-			'Stop Loss', 'Shares', 'Entry Price', 'Exit Price', 'Gain', 'Buy Order', 'Sell Order', 'Notes', 'Comments', 'Metadata', 'Buy Metadata','Sale Metadata'])
+			'Stop Loss', 'Shares', 'Entry Price', 'Exit Price', 'Gain', 'Buy Order', 'Sell Order', 'Notes', 'Comments', '', 'Buy Metadata','Sale Metadata'])
+		prep = ss.createSheet('Preparation')
+		prep.updateRow(1, ['Ticker', 'Type', 'Entry Price', 'Exit Price', 'Stop Loss', 'Notes', 'Expiration in Days', 'Amount', 'Sell At End of Day'])
 		return ss
 
 	def get_queued_trades(self):
@@ -43,23 +45,22 @@ class TradeJournal():
 		self.journal.refresh()
 		self.journal[0].updateRows([headerRow])
 
-	def create_queued_trade(self, row_num, ticker, type, entry, exit, stop_loss, notes, expiration, metadata, end_of_day):
+	def create_queued_trade(self, row_num, ticker, type, entry, exit, stop_loss, notes, expiration, amount, end_of_day):
 		self.journal.refresh()
-		self.journal[0].updateRow(row_num, [ticker, type, entry, exit, stop_loss, notes, expiration, metadata, end_of_day])
+		self.journal[0].updateRow(row_num, [ticker, type, entry, exit, stop_loss, notes, expiration, amount, end_of_day])
 
 
-	def create_trade_record(self, trade, notes, metadata):
+	def create_trade_record(self, trade, notes):
 		self.journal.refresh()
 		self.journal[1].updateRow(trade.id + 1, [trade.id, trade.create_date, trade.ticker, trade.type, trade.status, trade.entry_date, trade.exit_date, 
 			trade.planned_entry_price, trade.planned_exit_price, trade.stop_loss, trade.shares, trade.actual_entry_price, 
-			trade.actual_exit_price, '', trade.buy_order_id, trade.sell_order_id, notes, '', metadata, '', ''])
+			trade.actual_exit_price, '', trade.buy_order_id, trade.sell_order_id, notes, '', '', '', ''])
 
 	def update_trade_record(self, trade, sale_metadata=None, buy_metadata=None):
 		self.journal.refresh()
 		row = self.journal[1].getRow(trade.id + 1)
 		notes = row[16]
 		comments = row[17]
-		metadata = row[18]
 		gain = False
 
 		if sale_metadata == None:
@@ -73,4 +74,4 @@ class TradeJournal():
 
 		self.journal[1].updateRow(trade.id + 1, [trade.id, trade.create_date, trade.ticker, trade.type, trade.status, trade.entry_date, trade.exit_date, 
 			trade.planned_entry_price, trade.planned_exit_price, trade.stop_loss, trade.shares, trade.actual_entry_price, 
-			trade.actual_exit_price, gain, trade.buy_order_id, trade.sell_order_id, notes, comments, metadata, buy_metadata, sale_metadata])
+			trade.actual_exit_price, gain, trade.buy_order_id, trade.sell_order_id, notes, comments, '', buy_metadata, sale_metadata])
